aef0b303ffcf43a9ddc3733f38713034750dc4d5
Sergey Klimov <ksiu@pkzdrav.ru>
2024-01-22 16:41:09 +0700

    Добавлена проверка pylint

---
+++ market/products/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Product(models.Model):
+    """Продукт"""
+
+    name = models.CharField(max_length=512, verbose_name=_("наименование"))
+    details = models.JSONField(verbose_name=_("характеристики"))
