d9106d923554249c91abef676df6dced758845dd
Джозеф <joy.life86@mail.ru>
2024-02-01 15:57:00 +0300

    add model catigories, and categories.json

--- market/products/models.py
+++ market/products/models.py
@@ -1,3 +1,4 @@
+"""Django-модель, представляющая продукт."""
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -7,3 +8,24 @@ class Product(models.Model):
 
     name = models.CharField(max_length=512, verbose_name=_("наименование"))
     details = models.JSONField(verbose_name=_("характеристики"))
+
+
+class Category(models.Model):
+    """Модель категорий товара"""
+
+    name = models.CharField(max_length=512, verbose_name=_("наименование"), unique=True)
+    description = models.TextField(verbose_name=_("описание"), blank=True)
+    parent = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    sort_index = models.CharField(verbose_name=_("индекс сортировки"), blank=True)
+
+    class Meta:
+        db_table = "category"
+        verbose_name = _("category")
+        verbose_name_plural = _("categories")
+
+    class MPTTMeta:
+        order_insertion_by = ["name"]
+
+    def __str__(self):
+        return f"{self.name}"
