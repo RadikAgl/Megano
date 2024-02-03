0acaa3768fc20ebd9445f045e2f4b3bf2650293e
Notf8 <notf8@yandex.ru>
2024-02-03 11:00:26 +0300

    added test case for banner

--- market/products/tests/test_models.py
+++ market/products/tests/test_models.py
@@ -1,5 +1,33 @@
 from django.test import TestCase
-from products.models import Product
+from products.models import Product, Banner
+
+
+class BannerModelTest(TestCase):
+    """Класс тестов модели баннера"""
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.banner = Banner.objects.create(
+            name="Тестовый баннер",
+            actual=True,
+            preview="",
+        )
+
+    def test_verbose_name(self):
+        banner = Banner()
+        field_verboses = {
+            "name": "название",
+            "actual": "актуальность",
+            "preview": "превью",
+        }
+        for field, expected_value in field_verboses.items():
+            with self.subTest(field=field):
+                self.assertEqual(banner._meta.get_field(field).verbose_name, expected_value)
+
+    def test_name_max_length(self):
+        banner = BannerModelTest.banner
+        max_length = banner._meta.get_field("name").max_length
+        self.assertEqual(max_length, 512)
 
 
 class ProductModelTest(TestCase):
