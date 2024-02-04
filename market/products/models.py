88a06ca829db9fffe2622c27bbb06305317fbdd7
Notf8 <notf8@yandex.ru>
2024-02-04 12:56:24 +0300

    URLS field added to banner model

--- market/products/models.py
+++ market/products/models.py
@@ -8,6 +8,7 @@ class Banner(models.Model):
     name = models.CharField(max_length=512, verbose_name=_("название"))
     actual = models.BooleanField(default=True, verbose_name=_("актуальность"))
     preview = models.ImageField(verbose_name=_("превью"), upload_to="img/preview", null=True, blank=True)
+    link = models.URLField(verbose_name=_("ссылка"), blank=True, unique=True, db_index=True)
 
 
 class Product(models.Model):
