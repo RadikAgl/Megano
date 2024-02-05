7f5de824bec2e3c1959ccc8b99c28bca27670ae5
Владимир Сергеев <volodiya-sergeev@yandex.ru>
2024-02-05 08:42:57 +0300

    Добавление модели продавца и детальной страницы продавца.

--- market/shops/models.py
+++ market/shops/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import User
 
 
 class Shop(models.Model):
@@ -14,12 +15,28 @@ class Shop(models.Model):
     )
 
 
+class Seller(models.Model):
+    """Модель django orm продавца"""
+
+    name = models.CharField(max_length=512, verbose_name=_("Название продавца"))
+    contact_info = models.CharField(max_length=512, verbose_name=_("Контактная информация"))
+    description_of_seller = models.TextField(verbose_name=_("Описание"))
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name=_("Пользователь"))
+
+    class Meta:
+        verbose_name_plural = _("Продавцы")
+
+    def __str__(self):
+        return f"{self.name}"
+
+
 class Offer(models.Model):
     """Предложение магазина"""
 
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
     product = models.ForeignKey("products.Product", on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_("цена"))
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE, verbose_name="Продавец")
 
     class Meta:
         constraints = [models.UniqueConstraint("shop", "product", name="unique_product_in_shop")]
--- market/shops/views.py
+++ market/shops/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render  # noqa F401
+from django.shortcuts import render, get_object_or_404
+from .models import Seller
 
-# Create your views here.
+
+def seller_detail(request, seller_id):
+    """Функция отображает  страницу с детальной информацией о продавце"""
+
+    seller = get_object_or_404(Seller, pk=seller_id)
+    return render(request, "seller_detail.html", {"seller": seller})
