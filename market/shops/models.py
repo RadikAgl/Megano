28031bb15db3131aca01198806e0aa6f3b0d0f79
Владимир Сергеев <volodiya-sergeev@yandex.ru>
2024-02-07 06:08:38 +0300

    Adding and registering the "Shop" model in the administrative panel.

--- market/shops/models.py
+++ market/shops/models.py
@@ -1,17 +1,43 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import FileExtensionValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
-class Shop(models.Model):
-    """Магазин"""
+def shop_logo_directory_path(instance: "Shop", filename: str) -> str:
+    """Функция, гененрирующая путь, по которому будет храниться логотип компании-продавца"""
+    return "shops/shop_{name}/{filename}".format(
+        name=instance.name,
+        filename=filename,
+    )
 
-    name = models.CharField(max_length=512, verbose_name=_("название"))
+
+class Shop(models.Model):
+    """Модель django orm магазина, который является продавцом на маркетплейсе Megano"""
+
+    name = models.CharField(max_length=512, verbose_name=_("Название"))
+    contact_info = models.CharField(max_length=512, verbose_name=_("Контактная информация"))
+    description_of_seller = models.TextField(verbose_name=_("Описание"))
     products = models.ManyToManyField(
         "products.Product",
         through="Offer",
         related_name="shops",
-        verbose_name=_("товары в магазине"),
+        verbose_name=_("Товары в магазине"),
     )
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, verbose_name=_("Пользователь"))
+    logo = models.ImageField(
+        upload_to=shop_logo_directory_path,
+        validators=[FileExtensionValidator(allowed_extensions=["jpg", "jpeg", "png"])],
+        verbose_name=_("Логотип компании"),
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name_plural = _("Продавцы")
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class Offer(models.Model):
