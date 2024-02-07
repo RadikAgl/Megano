cc558eea487559d410b63882ec86a8c505fceb66
Джозеф <joy.life86@mail.ru>
2024-02-07 12:04:03 +0300

    realized comments

--- market/products/models.py
+++ market/products/models.py
@@ -1,5 +1,4 @@
 """Django-модель, представляющая продукт."""
-import json
 
 from django.db import models
 from django.utils.translation import gettext_lazy as _
@@ -17,7 +16,7 @@ class Category(models.Model):
         return self.product_set.exists()
 
     class Meta:
-        verbose_name_plural = _("категорий")
+        verbose_name_plural = _("категория")
 
     def __str__(self):
         return f"{self.name}"
@@ -33,21 +32,7 @@ class Product(models.Model):
     details = models.JSONField(default=dict, blank=True, verbose_name=_("детали"))
 
     class Meta:
-        verbose_name_plural = _("продукты")
+        verbose_name_plural = _("продукт")
 
     def __str__(self):
         return f"{self.name}"
-
-    def set_details(self, details_dict):
-        self.details = details_dict
-        self.save()
-
-    def get_details(self):
-        return self.details
-
-    def set_details_from_json(self, details_json):
-        try:
-            details_dict = json.loads(details_json)
-            self.set_details(details_dict)
-        except json.JSONDecodeError as e:
-            print(f"Error decoding JSON: {e}")
--- market/products/tests/test_models.py
+++ market/products/tests/test_models.py
@@ -1,6 +1,5 @@
-from django.core.management import call_command
 from django.test import TestCase
-from django.utils.translation import gettext as _
+
 from products.models import Product, Category
 
 
@@ -15,20 +14,7 @@ class ProductModelTest(TestCase):
 
     def test_fixture_loading(self):
         products_count = Product.objects.count()
-        self.assertGreater(products_count, 0)
-
-    def test_verbose_name(self):
-        field_verboses = {
-            "name": _("наименование"),
-            "category": _("категория"),
-            "description": _("описание"),
-            "created_at": _("дата создания"),
-            "details": _("детали"),
-        }
-
-        for field, expected_value in field_verboses.items():
-            with self.subTest(field=field):
-                self.assertEqual(self.product._meta.get_field(field).verbose_name, expected_value)
+        self.assertEqual(products_count, 54)
 
     def test_name_max_length(self):
         max_length = self.product._meta.get_field("name").max_length
@@ -40,11 +26,6 @@ class CategoryModelTest(TestCase):
 
     fixtures = ["05-categories.json"]
 
-    @classmethod
-    def setUpTestData(cls):
-        super().setUpTestData()
-        call_command("loaddata", "05-categories.json")
-
     def setUp(self):
         self.category = Category.objects.get(pk=1)
 
@@ -52,26 +33,6 @@ class CategoryModelTest(TestCase):
         category_count = Category.objects.count()
         self.assertEqual(category_count, 20)
 
-    def test_verbose_name(self):
-        field_verboses = {
-            "name": "наименование",
-            "sort_index": "индекс сортировки",
-        }
-        for field, expected_value in field_verboses.items():
-            with self.subTest(field=field):
-                self.assertEqual(self.category._meta.get_field(field).verbose_name, expected_value)
-
     def test_name_max_length(self):
         max_length = self.category._meta.get_field("name").max_length
         self.assertEqual(max_length, 512)
-
-
-class LoadFixturesTest(TestCase):
-    fixtures = ["05-categories.json", "07-products.json"]
-
-    def test_loaded_data(self):
-        category_count = Category.objects.count()
-        product_count = Product.objects.count()
-
-        self.assertGreater(category_count, 0, _("No categories loaded from fixtures"))
-        self.assertGreater(product_count, 0, _("No products loaded from fixtures"))
