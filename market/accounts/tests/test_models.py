f670619e3c9e3131bbbf58a4bd7b17353ce6a0a7
Nikita <vazaxac@gmail.com>
2024-02-18 20:02:25 +0300

    f

--- market/accounts/tests/test_models.py
+++ market/accounts/tests/test_models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import Group
 
 
 class UserModelTest(TestCase):
-    """класс для тестов юзера"""
+    """класс для тестов юзераf"""
 
     fixtures = ["fixtures/01-groups.json", "fixtures/02-users.json"]
 
