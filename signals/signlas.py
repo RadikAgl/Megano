24e71c53829d69581b6361ff3d5b5dd510d47e2c
Джозеф <joy.life86@mail.ru>
2024-02-22 13:18:36 +0300

    added signals/signals.py, product detail ProductDetailViewTest in test, product.jinja2,and services.product.services edited products.urls, market.urls and market.settings, changed config.urls

---
+++ signals/signlas.py
@@ -0,0 +1,14 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from products.models import Product
+from products.services.product_services import invalidate_product_details_cache
+
+
+@receiver(post_save, sender=Product)
+def invalidate_product_cache(sender, instance, **kwargs):
+    """
+    Функция-получатель сигнала для сброса кэша при сохранении экземпляра Product.
+    """
+    product_id = instance.id
+    invalidate_product_details_cache(product_id)
