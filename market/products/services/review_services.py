480ed63dae8f314406375eff751483374a12ef60
Владимир Сергеев <volodiya-sergeev@yandex.ru>
2024-02-23 07:22:32 +0300

    1. Создание модели отзывов 2. Разработка функционала по добавлению отзыва и получения списка отзывов по товару.

---
+++ market/products/services/review_services.py
@@ -0,0 +1,53 @@
+from django.db.models import QuerySet
+from products.models import Review, Product
+from typing import Tuple, Any
+from django.contrib.auth import get_user_model
+from django.core.paginator import Paginator
+from django.http import HttpRequest
+
+User = get_user_model()
+
+
+class ReviewsService:
+    """Сервис для работы с отзывами"""
+
+    def __init__(self, request: HttpRequest, profile: User, product: Product):
+        self.request = request
+        self.product = product
+        self.profile = profile
+
+    def add(self, review: str) -> None:
+        """
+        Добавляет отзыв к товару
+        :param review: текст отзыва о товаре
+        :return: None
+        """
+        Review.objects.create(
+            user=self.profile,
+            product_id=self.product,
+            text=review,
+        )
+
+    def get_reviews_for_product(self) -> QuerySet[Review]:
+        """
+        Возвращает все отзывы о товаре
+        :return: список отзывов
+        """
+        reviews = (
+            Review.objects.filter(product=self.product)
+            .order_by("-created_at")
+            .select_related("user")
+            .select_related("product")
+        )
+        return reviews
+
+    def paginate(self, reviews: QuerySet[Review]) -> Tuple[Paginator, Any]:
+        """
+        Возвращает пагинатор для работы пагинации отзывов о товарах
+        :param reviews: список отзывов о товаре
+        :return: пагинатор и объект пагинации
+        """
+        paginator = Paginator(reviews, 3)
+        page_number = self.request.GET.get("page")
+        page_obj = paginator.get_page(page_number)
+        return paginator, page_obj
--- market/products/views.py
+++ market/products/views.py
@@ -1,7 +1,10 @@
 """ Представления приложения products """
 from django.views.generic import TemplateView
-
 from products.services import MainPageService
+from services.review_services import ReviewsService
+from django.core.handlers.wsgi import WSGIRequest
+from django.shortcuts import redirect
+from .forms import ReviewsForm
 
 
 class MainPageView(TemplateView):
@@ -14,3 +17,18 @@ class MainPageView(TemplateView):
         main_page_service = MainPageService()
         context["products"] = main_page_service.get_products()
         return context
+
+
+def add_review(request: WSGIRequest):
+    """
+    Добавляет отзыв о товаре
+    :param request: пост запрос
+    :return: обновляет страницу
+    """
+    if request.method == "POST":
+        form = ReviewsForm(request.POST)
+        if form.is_valid():
+            review = ReviewsService(request, request.user, request.POST["product"])
+            text = form.cleaned_data["text"]
+            review.add(review=text)
+    return redirect(request.META.get("HTTP_REFERER"))
