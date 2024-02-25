255f789e480581e8ab8ad999e7f219e2ae17ffb9
Notf8 <notf8@yandex.ru>
2024-02-25 22:46:01 +0300

    BANNERS_EXPIRATION_TIME remove to settings, MainPageService updated

--- market/products/services/mainpage_services.py
+++ market/products/services/mainpage_services.py
@@ -1,6 +1,7 @@
 from django.db.models import QuerySet
 from django.core.cache import cache
 from products.models import Product, Banner
+from django.conf import settings
 
 
 class MainPageService:
@@ -10,16 +11,14 @@ class MainPageService:
         """Самые продаваемые продукты"""
         return Product.objects.all().prefetch_related("images")
 
-    def banners_cache(self, expiration_time: int = 600) -> QuerySet:
+    def banners_cache(self) -> QuerySet:
         """
         Функция возвращает 3 случайных объекта модели Banner в кэшированном виде.
-
-        :param expiration_time: Время кэширования данных (10 минут)
         """
 
         cache_key = "banners_cache"
         data = cache.get(cache_key)
         if not data:
             data = Banner.objects.filter(actual=True).order_by("?")[:3]
-            cache.set(cache_key, data, expiration_time)
+            cache.set(cache_key, data, settings.BANNERS_EXPIRATION_TIME)
         return data
