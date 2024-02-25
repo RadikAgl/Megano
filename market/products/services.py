b52272c1f036cfa3ada0af5d5a6dddcf7f546b34
Notf8 <notf8@yandex.ru>
2024-02-25 15:59:15 +0300

    Template 'products/index.jinja2', services and views files has updated

--- market/products/services.py
+++ market/products/services.py
@@ -1,6 +1,6 @@
 from django.db.models import QuerySet
-
-from products.models import Product
+from django.core.cache import cache
+from products.models import Product, Banner
 
 
 class MainPageService:
@@ -9,3 +9,17 @@ class MainPageService:
     def get_products(self) -> QuerySet:
         """Самые продаваемые продукты"""
         return Product.objects.all().prefetch_related("images")
+
+    def banners_cache(self, expiration_time: int = 600) -> QuerySet:
+        """
+        Функция возвращает 3 случайных объекта модели Banner в кэшированном виде.
+
+        :param expiration_time: Время кэширования данных (10 минут)
+        """
+
+        cache_key = "banners_cache"
+        data = cache.get(cache_key)
+        if not data:
+            data = Banner.objects.all().filter(actual=True).order_by("?")[:3]
+            cache.set(cache_key, data, expiration_time)
+        return data
--- market/products/views.py
+++ market/products/views.py
@@ -1,16 +1,17 @@
 """ Представления приложения products """
-from django.views.generic import TemplateView
 
+from django.views.generic import TemplateView
 from products.services import MainPageService
 
 
 class MainPageView(TemplateView):
     """Класс представление главной страницы"""
 
-    template_name = "products/index.jinja2"
+    template_name: str = "products/index.jinja2"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         main_page_service = MainPageService()
         context["products"] = main_page_service.get_products()
+        context["banners"] = main_page_service.banners_cache()
         return context
