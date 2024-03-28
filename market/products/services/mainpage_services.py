390e4ed62b6db7d238f64fbb5abae4c5b8fb4555
Radik Agliamov <aglyam92@mail.ru>
2024-03-28 23:29:47 +0300

    banners fixed, main page products cache configured

--- market/products/services/mainpage_services.py
+++ market/products/services/mainpage_services.py
@@ -13,45 +13,67 @@ from shops.models import Offer
 class MainPageService:
     """Сервисы главной страницы"""
 
+    def get_cache_key(self, prefix) -> str:
+        """Генерация ключа кэша"""
+        return f"{prefix}_main_page"
+
     def get_top_products(self) -> QuerySet:
         """Самые продаваемые продукты"""
+        cache_key = self.get_cache_key("top_products")
+        result = cache.get(cache_key)
+        if result is None:
+            result = Offer.objects.order_by("remains").select_related("product")[:8]
+            cache.set(cache_key, result)
 
-        return Offer.objects.order_by("remains").select_related("product")[:8]
+        return result
 
     def get_top_categories(self) -> list[Any]:
         """Самые популярные категории"""
+        cache_key = self.get_cache_key("top_categories")
+        result = cache.get(cache_key)
+        if result is None:
+            categories = (
+                Category.objects.all()
+                .annotate(cnt=Count("category"))
+                .annotate(min_price=Min("product__offer__price"))
+                .annotate(remains=Sum("product__offer__remains"))
+                .filter(cnt=0)
+                .order_by("remains")
+            )
+            result = [category for category in categories if category.is_active()][:3]
+            cache.set(cache_key, result)
 
-        categories = (
-            Category.objects.all()
-            .annotate(cnt=Count("category"))
-            .annotate(min_price=Min("product__offer__price"))
-            .annotate(remains=Sum("product__offer__remains"))
-            .filter(cnt=0)
-            .order_by("remains")
-        )
-
-        return [category for category in categories if category.is_active()][:3]
+        return result
 
     def get_hot_offers(self) -> QuerySet:
         """Горячие предложения"""
 
-        products = (
-            Product.objects.all()
-            .annotate(cnt=Count("discount_products"))
-            .annotate(avg_price=Avg("offer__price"))
-            .filter(cnt__gt=0)
-        )
-
-        return random.sample(list(products), k=9)
+        cache_key = self.get_cache_key("hot_offers")
+        result = cache.get(cache_key)
+        if result is None:
+            products = (
+                Product.objects.all()
+                .annotate(cnt=Count("discount_products"))
+                .annotate(avg_price=Avg("offer__price"))
+                .filter(cnt__gt=0)
+            )
+            result = random.sample(list(products), k=9)
+            cache.set(cache_key, result)
+        return result
 
     def get_limited_products(self) -> QuerySet:
         """Ограниченные предложения"""
+        cache_key = self.get_cache_key("limited_products")
+        result = cache.get(cache_key)
+        if result is None:
+            result = (
+                Product.objects.annotate(avg_price=Avg("offer__price"))
+                .filter(is_limited=True)
+                .filter(is_product_of_the_day=False)
+            )[:16]
 
-        return (
-            Product.objects.annotate(avg_price=Avg("offer__price"))
-            .filter(is_limited=True)
-            .filter(is_product_of_the_day=False)
-        )[:16]
+            cache.set(cache_key, result)
+        return result
 
     def get_product_of_day(self) -> Product:
         """Товар дня"""
