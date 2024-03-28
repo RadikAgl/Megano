440d93767e5c94cc01d5bb365fbba9f9f192e283
Джозеф <joy.life86@mail.ru>
2024-03-28 17:44:48 +0300

    added l10n and i18n

--- market/accounts/models.py
+++ market/accounts/models.py
@@ -1,40 +1,38 @@
 """Django-модели, представляющие пользователей интернет-магазина."""
 
-from django.db import models
-from django.contrib.auth.models import AbstractUser
 from django.contrib.auth import get_user_model
-from products.models import Product
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 from django.utils.translation import gettext_lazy as _
+
+from products.models import Product
 
 
 class User(AbstractUser):
-    """модель пользователя"""
+    """Модель пользователя"""
 
-    username = models.CharField(
-        unique=False,
-        blank=True,
-    )
+    username = models.CharField(unique=False, blank=True, verbose_name=_("Имя пользователя"))
     USERNAME_FIELD = "email"
-    email = models.EmailField(unique=True)
+    email = models.EmailField(unique=True, verbose_name=_("Email"))
     REQUIRED_FIELDS = []
 
     class Meta:
-        """имя таблицы"""
-
         app_label = "accounts"
+        verbose_name = _("пользователь")
+        verbose_name_plural = _("пользователи")
 
 
 class ViewHistory(models.Model):
-    """Django-модель, представляющая историю просмотра товара пользователем."""
+    """Модель истории просмотра товаров пользователем"""
 
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name=_("покупатель"))
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name=_("пользователь"))
     product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name=_("товар"))
     view_date = models.DateTimeField(auto_now_add=True, verbose_name=_("дата просмотра"))
     view_count = models.IntegerField(default=0, verbose_name=_("количество просмотров"))
 
     class Meta:
-        verbose_name = "история просмотров"
-        verbose_name_plural = "истории просмотров"
+        verbose_name = _("история просмотров")
+        verbose_name_plural = _("истории просмотров")
 
     def __str__(self):
         return f"{self.user} - {self.product} - {self.view_date}"
