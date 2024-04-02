7539d2d944a1e7d4469a21f531c8e823e75a833e
Джозеф <joy.life86@mail.ru>
2024-04-02 19:12:00 +0300

    added l10n and i18n all templates

--- market/config/urls.py
+++ market/config/urls.py
@@ -24,6 +24,7 @@ from market.config import settings
 urlpatterns = []
 
 urlpatterns += i18n_patterns(
+    path("i18n/", include("django.conf.urls.i18n")),
     path("admin/settings_app/", include("settings_app.urls")),
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls", namespace="user")),
