04ee3ff02cad14da8a50e567451231006e430c11
@vladimir_sergeev <volodiya-sergeev@yandex.ru>
2024-04-17 16:05:19 +0300

    17.04. Добавление логотипа магазина

--- market/shops/admin.py
+++ market/shops/admin.py
@@ -1,14 +1,25 @@
 """Настройки админ панели приложения shops"""
 from django.contrib import admin  # noqa F401
+from django.utils.safestring import mark_safe
+
 from .models import Shop, Offer
 
 
 class ShopAdmin(admin.ModelAdmin):
     """Администратор для модели 'Магазин'."""
 
-    list_display = ("name", "contact_info", "description")
+    list_display = ("name", "contact_info", "description", "logo_display")
     list_filter = ("name", "user")
     search_fields = ("name", "contact_info", "description")
+
+    def logo_display(self, obj):
+        """Отображение текущего логотипа в админке."""
+        if obj.logo:
+            return mark_safe('<img src="{0}" width="100px" />'.format(obj.logo.url))
+        else:
+            return "No logo"
+
+    logo_display.short_description = "Логотип"
 
 
 admin.site.register(Shop, ShopAdmin)
